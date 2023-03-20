11cf578458c42734f22243c43402e67a16377c01
Network Administrator <networkadministrator@Mac-Pro.local>
2023-03-20 15:25:53 +0700

    responsive progress update

--- client/src/routes/plantations/+page.svelte
+++ client/src/routes/plantations/+page.svelte
@@ -3,32 +3,31 @@
 </svelte:head>
 
 <section class="h-full " >
-    
-    <div class="px-5 sm:px-16">
-        <div>
-            <img class="sm:pt-0 pt-4 pb-2 sm:pb-0" src="image 30.png" alt="mushroom plantation">
+    <div class="px-5 lg:px-16">
+        <div class="2xl:flex  2xl:justify-center 2xl:items-center">
+            <img class="lg:pt-0 pt-4 pb-2 lg:pb-0 2xl:w-[100%]  " src="pexel-plantation.jpg" alt="mushroom plantation">
         </div>
-        <div class="flex flex-col-reverse sm:flex-row justify-between sm:my-24">
-			<div class="flex flex-col  sm:w-[30%] sm:mt-6 mt-4">
-                <div class="font-Montserrat text-sm sm:text-base ">Established in 1998, Panggung Aneka Boga is the largest processor of fresh chili peppers in Indonesia, producing over 10,000 MT per year of chili paste for FMCG companies in Indonesia. PAB is also the single largest purchaser of fresh chili peppers from local farmers, giving these subsistence farmers a needed source of income.</div>
-                <div class="text-[#1A83E3] font-lato font-bold text-2xl sm:text-4xl py-2 sm:py-8">10,000 MT per year</div>
-                <div class="font-Montserrat text-sm sm:text-base ">
+        <div class="flex flex-col-reverse lg:flex-row justify-between lg:my-24">
+			<div class="flex flex-col  lg:w-[30%] lg:mt-6 mt-4">
+                <div class="font-Montserrat text-sm lg:text-base ">Established in 1998, Panggung Aneka Boga is the largest processor of fresh chili peppers in Indonesia, producing over 10,000 MT per year of chili paste for FMCG companies in Indonesia. PAB is also the single largest purchaser of fresh chili peppers from local farmers, giving these subsistence farmers a needed source of income.</div>
+                <div class="text-[#1A83E3] font-lato font-bold text-2xl lg:text-4xl py-2 lg:py-8">10,000 MT per year</div>
+                <div class="font-Montserrat text-sm lg:text-base ">
                     <p>
                         PAB produces thermally and chemically preserved paste derived primarily from locally farmed fresh chili pepper sources. PAB will continue to pursue a low carbon footprint and support local farmers by sourcing chili peppers from local farmers.
                     </p>
                 </div>
 			</div>
-			<div class="flex sm:justify-end sm:w-[70%]">
-				<img src="image 43.png" alt="mushroom farm " class="sm:w-[60%] sm:my-0 mt-10" >
+			<div class="flex lg:justify-end lg:w-[70%]">
+				<img src="image 43.png" alt="mushroom farm " class="lg:w-[60%] lg:my-0 mt-10 w-full" >
 			</div>
 		</div>
-        <div class="flex flex-col sm:flex-row my-10 sm:my-2 w-[100%]">
-            <div class="flex  sm:w-[50%]">
-				<img src="image 6.png" alt="mushroom picking " class="sm:h-[80%]" >
+        <div class="flex flex-col lg:flex-row my-10 lg:my-2 w-[100%]">
+            <div class="flex  lg:w-[50%]">
+				<img src="image 6.png" alt="mushroom picking " class="lg:h-[80%] w-[100%]" >
 			</div>
-			<div class="flex flex-col sm:ml-24 sm:w-[40%] sm:mt-2">
-                <div class="text-[#1A83E3] font-lato font-bold text-2xl sm:text-4xl py-2 sm:py-8">Mushroom production capacity: 60MT/month or 2MT/day</div>
-                <div class="font-Montserrat text-sm sm:text-base">
+			<div class="flex flex-col lg:ml-24 lg:w-[40%] lg:mt-2">
+                <div class="text-[#1A83E3] font-lato font-bold text-2xl lg:text-4xl py-2 lg:py-8">Mushroom production capacity: 60MT/month or 2MT/day</div>
+                <div class="font-Montserrat text-sm lg:text-base">
                     <p>
                         Indo Evergreen’s highland vegetable farm and cooperatives provide natural, organic, and pesticide-free produce that is grown in a traditional cultivation methods along with a greenhouse hydroponic cultivation system. Hydroponic systems eliminate the need for toxic pesticides, and allows IEG to grow their crops in a clean mountain environment that is distant enough from industrial pollution, but close enough to be local and environmentally sustainable. As a result, fresh salad greens and herbs like Italian basil and coriander that are straight from the farm to your door, with no middleman, can be enjoyed. Plus, IEG possesses its own cold chain logistics so you can be sure your produce stays fresh all the way from farm to table. From the farmer to our stringent quality control, then to you.
                     </p>
@@ -36,10 +35,10 @@
 			</div>
 	
 		</div>
-        <div class="flex flex-col w-full  ">
-            <div class=" flex flex-row my-4 sm:my-0  gap-4  w-full sm:mb-4">
+        <div class="flex flex-col w-full 2xl:justify-center 2xl:items-center  ">
+            <div class=" flex flex-row my-4 lg:my-0 2xl:justify-center 2xl:items-center  gap-4  w-full lg:mb-4">
                 <div>
-                    <img src="image 28.png" alt="mushroom packing"  class="shrink" >
+                    <img src="image 28.png" alt="champignon mushroom packing"  class="shrink" >
                 </div>
                 <div>
                     <img src="image 31.png" alt="tomato" class="">
@@ -48,12 +47,12 @@
                     <img src="image 32.png" alt="organic vegetable" class="">
                 </div>
             </div>
-            <div >
-                <img src="image 33.png" alt="eggs">
+            <div class="flex 2xl:justify-center 2xl:items-center" >
+                <img src="image 33.png" alt="champignon mushroom" class="2xl:w-[98%]">
             </div>
 
         </div>
-        <div class="sm:my-24 my-16">
+        <div class="lg:my-24 my-16  2xl:flex 2xl:justify-center 2xl:items-center">
             <img src="Group 28.png" alt="factory address map">
         </div>
     </div>
